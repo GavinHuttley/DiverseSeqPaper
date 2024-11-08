61d7e802b6919e6c490a201a60d56bcc5a22934c
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-11-08 11:41:48 +1100

    ENH: added script for benchmark of ctree and results
    [NEW] results from running script on my MacBook Pro (M2 Max) are in tsv file
        Updated notebook produces figure. That performance will be improved when
        cogent3 implements support for drivers!
    
    [CHANGED] use new figure, plus other amendments to paper

--- paper/nbks/benchmark_ctree.py
+++ paper/nbks/benchmark_ctree.py
@@ -1,45 +1,58 @@
 from pathlib import Path
-import argparse
 import cogent3
+import itertools
 from diverse_seq import cli as dvs_cli
 from diverse_seq import util as dvs_util
 from click.testing import CliRunner
 from benchmark import TempWorkingDir, TimeIt
-from rich.progress import Progress
+from rich import progress as rich_progress
 
 RUNNER = CliRunner()
 
 
-def time_big_dataset():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file_path", type=str, help="dvseqs file")
-
-    args = parser.parse_args()
-    in_file = Path(args.file_path)
-
+def main():
+    in_file = Path("../data/soil.dvseqs").absolute()
     assert in_file.exists()
-    times = {"time": []}
-    with Progress() as track:
-        task = track.add_task("Running ctree", total=5)
-        for _ in range(5):
-            with TempWorkingDir() as temp_dir:
-                out_tree_file = temp_dir / f"{in_file.name}.tre"
-                args = f"-s {in_file} -o {out_tree_file} -k 12 --sketch-size 3000 -hp".split()
-                with TimeIt() as timer:
-                    r = RUNNER.invoke(dvs_cli.ctree, args, catch_exceptions=False)
-                    assert r.exit_code == 0, r.output
-                times["time"].append(timer.get_elapsed_time())
-                track.update(task, advance=1, refresh=True)
+    reps = [1, 2, 3]
+    kmer_sizes = list(range(10, 17, 2))
+    num_seqs = [50, 100, 150, 200]
 
-    table = cogent3.make_table(data=times)
-    outpath = f"benchmark_ctree_{in_file.stem}.tsv"
+    results = {"k": [], "numseqs": [], "time(s)": []}
+    with dvs_util.keep_running():
+        with rich_progress.Progress(
+            rich_progress.TextColumn("[progress.description]{task.description}"),
+            rich_progress.BarColumn(),
+            rich_progress.TaskProgressColumn(),
+            rich_progress.TimeRemainingColumn(),
+            rich_progress.TimeElapsedColumn(),
+        ) as progress:
+            repeats = progress.add_task("Doing reps", total=len(reps))
+            for _ in reps:
+                combo = progress.add_task(
+                    "Doing k x num_seq", total=len(num_seqs) * len(kmer_sizes)
+                )
+                for k, num_seq in itertools.product(kmer_sizes, num_seqs):
+                    with TempWorkingDir() as temp_dir:
+                        out_tree_file = temp_dir / f"{in_file.name}.tre"
+                        args = f"-s {in_file} -o {out_tree_file} -k {k} --distance mash --sketch-size 3000 -hp --limit {num_seq}".split()
+                        with TimeIt() as timer:
+                            r = RUNNER.invoke(
+                                dvs_cli.ctree, args, catch_exceptions=False
+                            )
+                            assert r.exit_code == 0, r.output
+                        results["time(s)"].append(timer.get_elapsed_time())
+                        results["k"].append(k)
+                        results["numseqs"].append(num_seq)
+                    progress.update(combo, advance=1, refresh=True)
+
+                progress.remove_task(combo)
+                progress.update(repeats, advance=1, refresh=True)
+
+    table = cogent3.make_table(data=results)
+    outpath = "benchmark-ctree.tsv"
     table.write(outpath)
-    dvs_util.print_colour(
-        f"mean(sec)={table.columns["time"].mean():.2f}; stdev(sec)={table.columns["time"].std(ddof=1):.2f}",
-        "blue",
-    )
     dvs_util.print_colour(f"Wrote {outpath}!", "green")
 
 
 if __name__ == "__main__":
-    time_big_dataset()
+    main()
